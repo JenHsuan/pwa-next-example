a8700bca4eb43920303f95e575ef0760662a3214
Sean Hsieh <sean.hsieh@garmin.com>
2020-09-24 16:21:05 +0800

    [debug]
    Change-Id: I993acc6aff8435667f271db619173280d0add246

--- sw.js
+++ sw.js
@@ -3,11 +3,39 @@ if (typeof importScripts === 'function') {
     /* global workbox */
     if (workbox) {
       console.log('Workbox is loaded');
-      workbox.core.skipWaiting();
-  
       /* injection point for manifest files.  */
       workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-     
+
+      workbox.core.clientsClaim();
+      workbox.core.skipWaiting();
+  
+      workbox.precaching.cleanupOutdatedCaches();
+
+      //fecth
+      var dataCacheName = 'Fetch'
+      self.addEventListener('fetch', event => {
+        if (!(event.request.url.indexOf('http') === 0)) {
+          return; 
+        }
+
+        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin'){
+          return;
+        }
+
+        console.log('[Service Worker] Fetching something...', event);
+        event.respondWith(
+          caches.match(event.request)
+          .then(cachedRes =>{
+              return cachedRes || fetch(event.request).then(res => {
+                caches.open(dataCacheName)
+                .then(cache => {
+                  cache.put(event.request, res.clone());
+                  return res;
+                })
+              })
+          })
+        );
+      });
       
     } else {
       // console.log('Workbox could not be loaded. No Offline support');
