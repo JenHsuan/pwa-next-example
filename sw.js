c6ab69f24747483ad7a3cd88423686bccbf59923
Sean Hsieh <sean.hsieh@garmin.com>
2020-09-24 09:02:47 +0800

    [debug] remove fetch event
    Change-Id: I8bb3b6a0bf837838643a8ba1f66e002f2a76e648

--- sw.js
+++ sw.js
@@ -8,32 +8,7 @@ if (typeof importScripts === 'function') {
       /* injection point for manifest files.  */
       workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
      
-      //fecth
-      var dataCacheName = 'Fetch'
-      self.addEventListener('fetch', event => {
-        if (!(event.request.url.indexOf('http') === 0)) {
-          return; 
-        }
-
-        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin'){
-          return;
-        }
-
-        console.log('[Service Worker] Fetching something...', event);
-        event.respondWith(
-          caches.match(event.request)
-          .then(cachedRes =>{
-              return cachedRes || fetch(event.request).then(res => {
-                caches.open(dataCacheName)
-                .then(cache => {
-                  cache.put(event.request, res.clone());
-                  return res;
-                })
-              })
-          })
-        );
-      });
-
+      
     } else {
       // console.log('Workbox could not be loaded. No Offline support');
     }
